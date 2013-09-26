54c7082fd9071c0e73845b08625ae2aa3c1c21a4
mengci.liu <sharon@mengcilius-MacBook-Pro.local>
2013-09-26 16:37:03 -0400

    first commit

---
+++ MusicSiteDB/guitarList/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Email(models.Model):
+	email_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+	source_type = models.CharField(max_length=200)
+	country = models.CharField(max_length=200)
+	state = models.CharField(max_length=200)
+	city = models.CharField(max_length=200)
