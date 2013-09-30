258ab41ba491ac0cfe8146876f030efa85c070d0
Chen Xu <xuchen81@Chens-MacBook-Pro.local>
2013-09-30 10:41:08 -0400

    changed the UI

--- MusicSiteDB/static/js/emailRegistration.js
+++ MusicSiteDB/static/js/emailRegistration.js
@@ -1,7 +1,11 @@
 $(function() {
-  // todo
-  // submit button click event. 
-  // your ajax call should be here.
+  $("#save-email-btn").click(function() {
+    // todo
+    // submit button click event. 
+    // your ajax call should be here.
+    // now it just print 123.
+  	console.log(123);
+  });
 });
 
 
