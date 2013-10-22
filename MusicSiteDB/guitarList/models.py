519f6f794e08603c46854914481c2cf1db7aa64b
mengci.liu <sharon@mengcilius-MacBook-Pro.local>
2013-10-21 23:09:33 -0400

    Made email field nullable

--- MusicSiteDB/guitarList/models.py
+++ MusicSiteDB/guitarList/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Email(models.Model):
 	email_id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=200)
-	email = models.CharField(max_length=200)
+	email = models.CharField(max_length=200, null=True)
 	source_type = models.CharField(max_length=200)
 	country = models.CharField(max_length=200)
 	state = models.CharField(max_length=200)
