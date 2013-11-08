4e253b8b63deda6d62177735b3bd3d7da16d0f4b
mengci.liu <sharon@mengcilius-MacBook-Pro.local>
2013-11-08 08:53:57 -0500

    search

--- MusicSiteDB/guitarList/models.py
+++ MusicSiteDB/guitarList/models.py
@@ -13,9 +13,35 @@ class Email(models.Model):
 	created_date = models.DateTimeField('date published', auto_now=True)
 
 	def __unicode__(self):
-		return self.email
+		if self.email is None:
+			return "NULL"
+		else:
+			return unicode(self.email) 
+		
 
 	class Meta:
 		db_table = "emails"
 		ordering = ["email_id"]
+
+class Tag(models.Model):
+	tag_id = models.AutoField(primary_key = True)
+	tag_name = models.CharField(max_length = 200)
+	tag_count = models.IntegerField(default = 0)
+
+
+	class Meta:
+		db_table = "tags"
+		ordering = ["tag_id"]
+
+class Image(models.Model):
+	image_id = models.AutoField(primary_key = True)
+	image_name = models.CharField(max_length = 200)
+	tag_index = models.CharField(max_length = 800,null = True)
+
+	class Meta:
+		db_table = "images"
+		ordering = ["image_id"]
+
+ 
+	
 
